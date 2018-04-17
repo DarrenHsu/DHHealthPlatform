54e86176e5f3b661999f27f485e1b72a9a3bb286
Darren Hsu <chungen720@gmail.com>
2018-04-17 13:32:03 +0800

    update code

--- src/routes/api/LineWebhookAPI.ts
+++ src/routes/api/LineWebhookAPI.ts
@@ -1,7 +1,6 @@
 import * as mongoose from 'mongoose';
 import * as querystring from 'querystring';
 import * as JwtDecode from 'jwt-decode';
-import * as qs from 'qs';
 import Axios from 'axios';
 import { MiddlewareConfig, Client, middleware, JSONParseError, SignatureValidationFailed, TemplateMessage, WebhookEvent, ClientConfig, validateSignature, TextMessage } from '@line/bot-sdk';
 import { CONNECTION_CODE, MONGODB_CODE, ResultCodeMsg, LINE_CODE } from '../ResultCode';
@@ -183,12 +182,12 @@ export class LineWebhookAPI extends BaseAPI {
             var channelSecret = DHAPI.pkgjson.linelogin.channelSecret;
 
             /* Get Access Token */
-            var bodyFormData = qs.stringify({
+            var bodyFormData = {
                 'grant_type': 'authorization_code',
                 'code': code,
                 'redirect_uri': channelId,
                 'client_secret': channelSecret
-            });
+            };
 
             DHLog.ld('body ' + bodyFormData);
             
